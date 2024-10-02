663a9b580bb8a3e89f88dc363cc50ef4ce52edc3
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-10-02 16:12:34 +0300

    02.10

--- src/users/Users-app.jsx
+++ src/users/Users-app.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './Users-style.css';
 
 import Success from './Success';
 import User from './User';
 import Consumer from "./Consumer";
+import {json} from "react-router-dom";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function UsersApp() {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() =>{
+        fetch('https://reqres.in/api/users ')
+            .then(res => res.json())
+            .then((json) => {
+                setUsers(json.data);
+            }).catch(err => {
+                console.warn(err);
+                alert('Ошибка при получении пользователей')
+        })
+            .finally(() => setLoading(false));
+
+    }, []);
+
+    const onChangeSearchValue = (event) =>{
+        setSearchValue(event.target.value);
+
+    }
+
     return (
         <div className="Users-app">
-            <Consumer />
+            <Consumer
+                onChangeSearchValue={onChangeSearchValue}
+                searchValue={searchValue}
+                items={users}
+                isLoading={isLoading}/>
             {/* <Success /> */}
         </div>
     );
